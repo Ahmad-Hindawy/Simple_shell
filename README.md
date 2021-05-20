64fdf5a089c6d354cca1a897d290a5d5ba255c64
Ahmad-Hindawy <https://Ahmad-Hindawy@github.com>
2021-05-20 12:52:17 +0200

    First stable version Segmentation error fixed

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ This is a simple Unix shell written in C.
 - [Tutorial to code a simple shell in C](https://medium.com/swlh/tutorial-to-code-a-simple-shell-in-c-9405b2d3533e)
 - [GNU Readline Library](https://tiswww.case.edu/php/chet/readline/rltop.html#Documentation) 
 
+### TODO:
+- Add shell builtin functions
+- Add signal handler and "&" operator handling
+- Another implementation for reading and parsing command (Optional)
+- More testing and better documentation
--- shell.c
+++ shell.c
@@ -5,7 +5,60 @@
 #include<sys/types.h>
 #include<sys/wait.h>
 
+//these libraries are used to read line from stdin and also having a history of commands
+#include<readline/readline.h>
+#include<readline/history.h>
 
+#define MAX_BUF_SIZE 64 //maximum number of characters for any buffer
+#define DELIM " \t\r\n\a" //escape characters that seperates tokens
+
+char** parse_command(char *str ){
+	int buf_size = MAX_BUF_SIZE , pos = 0;
+	char **tokens_arr = malloc(buf_size * sizeof(char*));
+	char *token;
+
+	if(!tokens_arr){		//The malloc function did not complete
+		printf("\nAllocation error!");
+		exit(0);
+	}
+
+	token = strtok(str,DELIM);
+	while(token != NULL){
+		tokens_arr[pos] = token;
+		pos++;
+
+		if(pos >= buf_size){
+			buf_size += MAX_BUF_SIZE;
+			tokens_arr = realloc(tokens_arr , buf_size * sizeof(char*));
+			if(!tokens_arr){		//The malloc function did not complete
+				printf("\nAllocation error!");
+				exit(0);
+			}
+		}
+
+		token = strtok(NULL,DELIM);
+	}
+	
+	tokens_arr[pos] = NULL;
+	//*position_var = pos; //number of tokens + 1 for null termination
+	//printf("%d\n",pos);
+	return tokens_arr;
+	
+}
+
+
+int get_command(char *str){
+	char *buf;
+	buf = readline("\n>>>");
+	if (strlen(buf) != 0){
+		add_history(buf);
+		strcpy(str,buf);	
+		return 0;
+	}
+	else{
+		return 1;
+	}
+}
 
 // Executes the command given in an array of strings
 void execute(char **commands){
@@ -38,11 +91,23 @@ void execute(char **commands){
 
 
 int main() {
-	
-
-
+	char *input_str , **parsed_tokens;
+	int *pos = NULL;
+	int i =0;
 	while(1){
-		
+		if(get_command(input_str))
+			continue;
+		parsed_tokens = parse_command(input_str );
+                 /*while(parsed_tokens[i]!= NULL ){
+                         printf("%s\n",parsed_tokens[i]);
+                         i++;
+                 }*/
+
+		execute(parsed_tokens);
+		//free(input_str);
+		//free(parsed_tokens);
+		//pos = NULL;
+
 	}
 
 }
