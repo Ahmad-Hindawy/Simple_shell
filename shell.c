781f2dc283f089fd530d91864a8e4949ec972ace
Nourhan Sowar <48545560+NourhanSowar@users.noreply.github.com>
2021-06-02 22:41:44 +0200

    Update shell.c
    Latest_Commented_version

--- shell.c
+++ shell.c
@@ -12,11 +12,13 @@
 
 
 #define MAX_BUF_SIZE 64 //maximum number of characters for any buffer
-#define DELIM " \t\r\n\a" //escape characters that seperates tokens
-int flag = 0 ;
+#define DELIM " \t\r\n\a" //escape characters that separates tokensSS
 
+int flag = 0 ; //set flag to indicate the & parameter
+
+//---------function to parse command to separated tokens to obtain an array that contains the command and its arguments
 char** parse_command(char *str ){
-	int buf_size = MAX_BUF_SIZE , pos = 0;
+    int buf_size = MAX_BUF_SIZE , pos = 0;
 	char **tokens_arr = malloc(buf_size * sizeof(char*));
 	char *token;
 
@@ -24,33 +26,33 @@ char** parse_command(char *str ){
 		printf("\nAllocation error!");
 		exit(0);
 	}
-	token = strtok(str,DELIM);
+	token = strtok(str,DELIM);//spliting the command by DELM
 	while(token != NULL){
 		tokens_arr[pos] = token;
 		pos++;
 
 		if(pos >= buf_size){
-			buf_size += MAX_BUF_SIZE;
+            buf_size += MAX_BUF_SIZE;
 			tokens_arr = realloc(tokens_arr , buf_size * sizeof(char*));
+
 			if(!tokens_arr){		//The malloc function did not complete
 				printf("\nAllocation error!");
 				exit(0);
 			}
-		}
-
-		token = strtok(NULL,DELIM);
+        }
+		token = strtok(NULL,DELIM); //generate tokens according the DELIM entered by the user
 	}
-	tokens_arr[pos] = NULL;
-	//*position_var = pos; //number of tokens + 1 for null termination
-	//printf("%d\n",pos);
-	return tokens_arr;
+	tokens_arr[pos] = NULL; //make the last argument of the array NULL
 
+	return tokens_arr;
 }
+
+//-------function to get command from the user using readline library and save it as a history
 int get_command(char *str){
 	char *buf;
 	buf = readline("\n>>>");
 	if (strlen(buf) != 0){
-		add_history(buf);
+		add_history(buf);  //save the command in the history.By using the up arrow,you can get the last commands
 		strcpy(str,buf);
 		return 0;
 	}
@@ -59,104 +61,120 @@ int get_command(char *str){
 	}
 }
 
+//----------single Handler to write on the log file -----------------------------------
 void signal_handler (int sig) {
     printf("inside handler \n") ;
     FILE *fp;
-    fp  = fopen ("/home/mahmoud/Desktop/Unix_shell/data.log", "a");       // NOTE : should put fixed path to overcome any segmentation fault
+//---------*****IMP NOTE: should put fixed path to overcome any segmentation fault--------------
+    fp  = fopen ("/home/nourhan/Nourhan/OS/Unix_shell/data.log", "a");
     fprintf(fp, "\n Child process was terminated in signal handler \n");
     fclose(fp) ;
     return ;
 }
 
-// Executes the command given in an array of strings
+//---------------------Executes the command given in an array of tokens----------------------------
 void execute(char **commands){
 	int ret;
 	int i = 0 ;
     flag = 0 ;
     int stat;
-    //******* & parameter *********
+
+    //------handle & parameter--------------------------
+    // by comparing the elements of token_array,Once it is founded, set the flag to be 1
+    //and change the element by NULL
     while (commands[i] != NULL) {
         printf("commands= %s \n" , commands[i]) ;
         if (!strcmp(commands[i] , "&")){
-            flag = 1 ;
+            flag = 1 ; //set flag=1 indicating that this process will run in background
             commands[i] = NULL ;
             break ;
         }
         i++ ;
     }
-    //**********exit state***************
-    if(strcmp(commands[0], "exit") == 0){
 
+    //-------------handle exit command-----------------------------
+    //by comparing the elements of token_array,Once it is founded, print end message and close the terminal
+    if(strcmp(commands[0], "exit") == 0){
         printf("The Shell is exited now , Bye \n");
         exit(0);
     }
+
+    //--------------------handle cd command---------------------------
+    //by comparing the elements of token_array,Once it is founded,change directory to what the user entered as argument
     if(strcmp(commands[0], "cd") == 0){
         printf("inside chdir function");
         chdir(commands[1]) ;
     }
+
+//-----------------------------------child process creation and execution section-------------------------------
     else {
+        pid_t pid = fork();//forking new child process
 
+        if (pid == -1){		//failed to create child process
+            printf("\nProccess forking failed!");
+        }
 
+    //----------------------child Process Section---------------------------
+        else if (pid == 0){	//child proccess creation is succeeded
+            printf("we are in child process \n") ;
+            ret = execvp(commands[0],commands);      // execution the process
+            printf("excution is done\n");
 
-	//forking
-	pid_t pid = fork();
-	if (pid == -1){		//failed
-		printf("\nProccess forking failed!");
-	}
-	else if (pid == 0){	//child proccess
-        printf("we are in child process \n") ;
-		ret = execvp(commands[0],commands);            // execution
-		printf("excution is done\n");
-		if (ret< 0) {	//execusion failed
-			printf("\nCommand execusion failed!");
-		}
-		exit(0);
-	}
-	else{			//parent section
-        printf("we are in parent process \n") ;
-		/* The parent should wait for the child proccess to terminate or not.
+            if (ret< 0) {	//execution is failed
+                printf("\nCommand execution failed!");
+            }
+            exit(0);
+        }
+
+    //------------------------------parent section------------------------------------
+        /* The parent should wait for the child processes to terminate or not.
 		 * This is determined by the user providing & as a last parameter or not.
-		 * if given then the parent will NOT wait for the child to terminate and
-		 * vice versa*/
-		// TODO:handle the & parameter
-		if (flag == 1) {    // & is found in the command >> child must operate in bachground >> parent shouldn't wait for the child
-            flag = 0 ;
-            printf("child is operating in background \n ") ;
-           // signal(SIGCHLD , signal_handler ) ;
-		}
-        else if (flag == 0) {
-            printf("waiting in else if = 0 \n") ;
-           	//waits for the child to terminate
-            printf("First Wait\n");
+		 * if given,then the parent will NOT wait for the child to terminate
+		 */
+        else{
+            printf("we are in Parent Process \n") ;
 
+    //-------------------------BackGround Case--------------------------*
+            /* & is founded in the command,child must operate in background
+            /*parent should NOT wait for the child to terminate.*/
+            if (flag == 1) {
+                flag = 0 ; //restore the flag to the default value which is zero
+                printf("Child is operating in BackGround \n ") ;
+            }
+
+    //-------------------------ForeGround Case---------------------------*
+            else if (flag == 0) {
+                printf("Child is operating in ForeGround \n") ;
+                printf("Waiting.....\n");
+        //--------waiting the child process to terminate----------
+            /*WIFEXITED macro will be TRUE if the child terminated normally.
+            /*WEXITSTATUS macro is employed only if WIFEXITED returned true to
+            /*check status of child process*/
             waitpid(pid,&stat,0);
             if (WIFEXITED(stat)){
-               printf("Exit status: %d\n", WEXITSTATUS(stat));
-                printf("second Wait\n");
-
+                printf("Exit status: %d\n", WEXITSTATUS(stat));
+                printf(" Wait is DONE \n");
             }
-            printf("(parent) child process is exited \n") ;
-            //return ;
+        //---------------------Execution is DONE successfully ---------------------------
+            printf("(Parent) child process is exited \n") ;
            	}
-
-		return;
-	}
+        return;
+        }
 	}
 }
+//--------------------main function----------------------------------------------------------------
 int main() {
 
 	char *input_str , **parsed_tokens;
 	int *pos = NULL;
 	int i =0;
+	//Using SIGCHILD signal which is sent to the parent process once the child process stops or terminates
 	signal(SIGCHLD , signal_handler ) ;
+
 	while(1){
 		if(get_command(input_str))
 			continue;
 		parsed_tokens = parse_command(input_str );
-
-
-        printf("before execution in main \n") ;
 		execute(parsed_tokens);
 	}
 }
-
